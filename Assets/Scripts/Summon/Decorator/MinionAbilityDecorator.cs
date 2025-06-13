c6503bd71b4bc519bc8f539c05c20c273155a380
RyanDefost <70955105+RyanDefost@users.noreply.github.com>
2025-06-13 13:04:40 +0200

    added a single unsaved command

--- Assets/Scripts/Summon/Decorator/MinionAbilityDecorator.cs
+++ Assets/Scripts/Summon/Decorator/MinionAbilityDecorator.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace Project.Summon.Decorator
 {
+    /// <summary>
+    /// Assigns the ability to the given minionData.
+    /// </summary>
     public class MinionAbilityDecorator : MinionDecorator
     {
         private Dictionary<MinionType, IAbility> abilities = new Dictionary<MinionType, IAbility>()
@@ -15,6 +18,7 @@ namespace Project.Summon.Decorator
 
         public MinionAbilityDecorator(MinionType minionType) : base(minionType) { }
 
+        //Decorates the MinionData with the correlation values.
         public override MinionData Decorate(MinionData minionData)
         {
             minionData = SetAbility(minionData);
